f23aeabb92a0fcaa3d927172b5fe4a9b8caa34cd
cnxk <cos4338@gmail.com>
2022-09-29 07:33:29 +0900

    Feat: RTK Setting

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,17 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import updateSlice from './updateSlice';
+import inputSlice from './inputSlice';
+
+const rootReducer = combineReducers({
+  input: inputSlice,
+  list: updateSlice,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
+
+export type AppDispatch = typeof store.dispatch;
+export type ReducerType = ReturnType<typeof rootReducer>;
