abd7d2cce9948d64861e5e4f7c6a7c8303a910cb
jihyun-jeon <jhplus13@naver.com>
2022-09-28 11:55:06 +0900

    Chore : 초기셋팅, 폴더 생성

---
+++ src/store/searchSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState: {},
+  reducers: {
+    addKeywords: (state, action) => {
+      state[action.payload.id] = action.payload;
+    },
+  },
+});
+
+export const { addKeywords } = searchSlice.actions;
+export default searchSlice.reducer;
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "./searchSlice";
+
+const store = configureStore({
+  reducer: {
+    keywords: searchReducer,
+  },
+});
+
+export default store;
