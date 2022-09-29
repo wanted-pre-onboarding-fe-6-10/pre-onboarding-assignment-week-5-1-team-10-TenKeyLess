059a5f0cd3725b02c99c980f9314b2ccaebd8e58
윤관 <blackgar@naver.com>
2022-09-29 09:35:32 +0900

    deps: 불필요한 recoil recoil-persist삭제

--- src/components/searchBar/searchBar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const searchBar = () => {
-  return <div>searchBar</div>;
-};
-
-export default searchBar;
--- src/components/searchTable/searchTable.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const searchTable = () => {
-  return <div>searchTable</div>;
-};
-
-export default searchTable;
