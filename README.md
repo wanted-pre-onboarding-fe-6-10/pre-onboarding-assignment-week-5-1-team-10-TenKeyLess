083bcace890a9061c5d38474c19c3bd2c07d64bb
jihyun-jeon <jhplus13@naver.com>
2022-09-29 09:03:00 +0900

    Docs : 코드 컴포넌트 파일 분리, 리드미 작성

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pre-onboarding-assignment-week-5-1-team-10-TenKeyLess
+
 원티드 프리온보딩 프론트엔드 6차 10팀 추천 검색어 과제
+
+# 사용기술
+
+- 언어 : Javascript
+- 라이브러리 : React, React-router-dom
+- 상대관리 : redux , redux-toolkit , redux logger
+- styling : tailwind , react-icon
+
+# redux toolkit을 활용한 상태관리
+
+store에 모든 검색결과를 저장하고 있는 구조
+하나의 객체에 "검색키워드"와 "검색결과"가 키와 값으로 들어있음
+
+```
+keywords : {검색키워드 : 검색결과[] , ...}
+```
+
+# 로컬 캐싱 구현
+
+### store에 모든 검색어와 검색결과를 담아둔 뒤 있는 데이터면 api호출 없이 store값 쓰는 식으로 처리
+
+1. 검색어 input창에 타이핑
+2. 타입핑 완료 후 2초 뒤에 store에 저장해둔 데이터가 있는지 찾아봄
+3. 검색어에 해당하는 결과를 store에 담아둔게 있으면 api호출 없이 store값 사용
+4. store에 담아둔게 없다면 - api호출 > store에 값 저장 후 > api응답 데이터 사용
+
+# 키보드만으로 추천 검색어 이동기능
+
+1. 검색 완료 후 첫번째 검색결과 리스트에 포커스가 지정되도록 함. (useRef활용)
+2. 이후 해당 리스트의 id값과 변수 highLighNum값이 같으면 하이라이트 처리
+3. 아래 방향키 누를시 highLighNum값을 1씩 증가하여 다음번 리스트에 하이라이트 처리 (event.keyCode로 방향키 따짐)
+4. 위 방향키 누를시 highLighNum값을 1씩 증가하여 다음번 리스트에 하이라이트 처리 (event.keyCode로 방향키 따짐)
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
+/* eslint-disable no-console */
+import React, { useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addKeywords } from '../../store/keywordsSlice';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { BsInbox } from 'react-icons/bs';
-
-const BaseUrl = process.env.REACT_APP_BASE_URL; // [질문]
+import { getSickDataRequest } from '../../api/api';
+import { SearchBox, NoSearchBox } from './SearchBoxes';
+import { highlightedText } from '../../utils/hightLighting';
 
 const Home = () => {
+  const [inputValue, setinputValue] = useState('');
   const [keywordList, setKeywordList] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
 
   const dispatch = useDispatch();
   const { keywords } = useSelector(state => state);
@@ -20,150 +19,64 @@ const Home = () => {
     clearTimeout(decounceFlag.current);
     setKeywordList([]);
 
-    let searchValue = e.target.value;
-    setSearchValue(searchValue);
+    let inputValue = e.target.value;
+    setinputValue(inputValue);
 
-    if (searchValue === '') {
+    if (inputValue === '') {
       return;
     }
 
     decounceFlag.current = setTimeout(() => {
-      const searchList = keywords[searchValue];
+      const sickNameList = keywords[inputValue];
 
-      if (searchList) {
-        const bordList = searchList.map(el => highlightedText(el.sickNm, searchValue));
+      if (sickNameList) {
+        const bordList = sickNameList.map(el => highlightedText(el.sickNm, inputValue));
         setKeywordList(bordList);
       } else {
-        axios(`${BaseUrl}/sick?q=${searchValue}`).then(res => {
+        getSickDataRequest(inputValue).then(res => {
           console.log('api 호출 횟수 : ', apiCount);
           apiCount += 1;
 
           const responseList = res.data;
-          dispatch(addKeywords({ searchWord: searchValue, result: responseList }));
+          dispatch(addKeywords({ searchWord: inputValue, result: responseList }));
 
-          const bordList = responseList.map(el => highlightedText(el.sickNm, searchValue));
+          const bordList = responseList.map(el => highlightedText(el.sickNm, inputValue));
           setKeywordList(bordList);
         });
       }
-    }, 3000);
+    }, 2000);
   };
 
   return (
-    <div>
-      <header className="w-full h-[460px] flexCenter flex-col relative bg-mainbgColor mt-14">
-        <div className="w-2/5   flex flex-col items-center">
-          {/* 1. 제목 */}
-          <h1 className="text-2xl mb-10 font-bold text-mainColor  text-center leading-loose">
-            국내 모든 임상기험 검색하고 <br /> 온라인으로 참여하기
-          </h1>
-          {/* 2. 검색창 */}
-          <div className="mb-14 px-5 py-2 bg-white w-5/6 h-16 rounded-3xl flexCenter justify-between ">
-            <input
-              type="text"
-              value={searchValue}
-              className=" border-none w-4/5 text-xl"
-              placeholder="질환명을 입력해주세요"
-              onInput={e => keywordChange(e)}
-            />
-            <button type="button" className="btn-clear" onClick={() => setSearchValue('')}>
-              x
-            </button>
-            <button type="button" className="btn-primary">
-              검색
-            </button>
-          </div>
+    <header className="w-full h-[460px] flexCenter flex-col relative bg-mainbgColor mt-14">
+      <div className="w-2/5   flex flex-col items-center">
+        <h1 className="text-2xl mb-10 font-bold text-mainColor  text-center leading-loose">
+          국내 모든 임상기험 검색하고 <br /> 온라인으로 참여하기
+        </h1>
+
+        <div className="mb-14 px-5 py-2 bg-white w-5/6 h-16 rounded-3xl flexCenter justify-between ">
+          <input
+            type="text"
+            value={inputValue}
+            className=" border-none w-4/5 text-xl"
+            placeholder="질환명을 입력해주세요"
+            onInput={e => keywordChange(e)}
+          />
+          <button type="button" className="btn-clear" onClick={() => setinputValue('')}>
+            x
+          </button>
+          <button type="button" className="btn-primary">
+            검색
+          </button>
         </div>
-        {/* 3. 검색결과창 */}
-        {searchValue && keywordList.length > 0 ? (
-          <SearchBox keywordList={keywordList} />
-        ) : (
-          <NoSearchBox />
-        )}
-      </header>
-    </div>
+      </div>
+      {inputValue && keywordList.length > 0 && <SearchBox keywordList={keywordList} />}
+      {inputValue && keywordList.length === 0 && <NoSearchBox />}
+      {/* [TODO] 버그해결 - 검색어 입력 후 api기다리는 동안 바로 noSearchBox가 나와서, 실제 검색결과 있어도 noSearchBox를 한번씩 거치고 가는 버그 있음 */}
+    </header>
   );
 };
 
 export default Home;
 
 let apiCount = 1;
-const highlightedText = (text, searchWord) => {
-  if (searchWord !== '' && text.includes(searchWord)) {
-    const parts = text.split(new RegExp(`(${searchWord})`, 'gi'));
-
-    return (
-      <span>
-        {parts.map((part, index) => (part === searchWord ? <mark key={index}>{part}</mark> : part))}
-      </span>
-    );
-  }
-};
-
-const SearchBox = ({ keywordList }) => {
-  let [highLighNum, setHighLighNum] = useState(0);
-
-  const LinkEl = useRef();
-
-  useEffect(() => {
-    if (+LinkEl.current.id === +highLighNum) {
-      LinkEl.current.classList.add('bg-gray-200');
-      setTimeout(() => {
-        LinkEl.current.focus();
-      }, 100);
-    }
-  }, [highLighNum]);
-
-  return (
-    <div className="absolute top-[310px] w-2/6 h-max overflow-scroll scrollbar-hide rounded-3xl py-5 bg-gray-300">
-      <ul
-        className="w-full max-h-96"
-        onKeyDown={e => {
-          if (e.keyCode === 40) {
-            setHighLighNum(prev => {
-              if (prev === keywordList.length - 1) {
-                return prev;
-              }
-              return (prev += 1);
-            });
-          }
-          if (e.keyCode === 38) {
-            setHighLighNum(prev => {
-              if (prev === 0) {
-                return prev;
-              }
-              return (prev -= 1);
-            });
-          }
-        }}
-      >
-        <li className="pl-2">{keywordList.length}개의 검색 결과</li>
-        {keywordList.map((spanEl, idx) => {
-          return (
-            <li key={idx}>
-              {idx === highLighNum && (
-                <a href="/" ref={LinkEl} id={idx} className="searchList flex items-center">
-                  <AiOutlineSearch className="mr-3" />
-                  {spanEl}
-                </a>
-              )}
-              {idx !== highLighNum && (
-                <a href="/" id={idx} className="searchList flex items-center">
-                  <AiOutlineSearch className="mr-3" />
-                  {spanEl}
-                </a>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
-
-const NoSearchBox = () => {
-  return (
-    <div className="absolute top-[310px] w-2/6 h-max flexCenter rounded-3xl py-5 bg-gray-300">
-      <BsInbox className="mr-5" /> 검색 결과가 없습니다
-    </div>
-  );
-};
